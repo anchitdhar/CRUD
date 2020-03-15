360b87356805846f7f7a29e47099de8ebd5db339
anchitdhar <anchitdhar@yahoo.com>
2020-03-15 17:11:40 +0530

    first commit

---
+++ backend/backendapi/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+        
+    def create(self, validate_data):
+        user = User.objects.create_user(**validate_data)
+        return user
